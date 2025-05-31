1f7701ba7a4ad2da1388a236777a656bc8f7773f
EddieSmith <145656018+lordwee@users.noreply.github.com>
2025-05-31 12:39:26 +0200

    Added function playRound()
    Added the logic deciding the winner of one round

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
-console.log('Hello World!');
-
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 let humanScore = 0;
 let computerScore = 0;
 
@@ -9,11 +7,11 @@ let computerScore = 0;
 function getComputerChoice () {
     const number = Math.floor(Math.random() * 3);
     if (number === 0) {
-        return 'Rock';
+        return 'rock';
     } else if (number === 1) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 };
 
@@ -21,11 +19,34 @@ function getHumanChoice() {
     let promptText = prompt('Rock, Paper, or Scissors?');  
     return promptText;
 };
-// function playRound (, computerChoice) {
-//          (humanChoice === 'Rock')
-// }
-console.log(compChoice);
-console.log(humanChoice);
+
+function playRound (humanChoice, computerChoice) {
+        let winner;
+         if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            humanScore++;
+            return winner = "You win: Rock beats Scissors!! Your score: " + humanScore + " Computer's score: " + computerScore;
+         } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            humanScore++;
+            return winner = "You win: Paper beats Rock!! Your score: " + humanScore + " Computer's score: " + computerScore;
+         } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            humanScore++;
+            return winner = "You win: Scissors beats Paper!! Your score: " + humanScore + " Computer's score: " + computerScore;
+         } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            computerScore++;
+            return winner = "You lose: Rock beats Scissors!! Your score: " + humanScore + " Computer's score: " + computerScore;
+        } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+            computerScore++;
+            return winner = "You lose: Paper beats Rock!! Your score: " + humanScore + " Computer's score: " + computerScore;
+        } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+            computerScore++;
+            return winner = "You lose: Scissors beats paper!! Your score: " + humanScore + " Computer's score: " + computerScore;          
+        } else {
+            return winner = "No winner! Both chosen: " + humanSelection
+        }
+};
+
+console.log(playRound(humanSelection, computerSelection));
+console.log("You: " + humanSelection + " Computer: " + computerSelection);
 
 
 
