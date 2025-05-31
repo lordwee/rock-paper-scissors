6c920d02818aee642a906defb5d74da9f46cf1f6
EddieSmith <145656018+lordwee@users.noreply.github.com>
2025-05-31 11:55:48 +0200

    Added two functions
    One prompt for the user to choose rock paper or
    scissor
    One random number for the computer to choose one.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
 console.log('Hello World!');
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice () {
+    const number = Math.floor(Math.random() * 3);
+    if (number === 0) {
+        return 'Rock';
+    } else if (number === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+};
+
+function getHumanChoice() {
+    let promptText = prompt('Rock, Paper, or Scissors?');  
+    return promptText;
+};
+// function playRound (, computerChoice) {
+//          (humanChoice === 'Rock')
+// }
+console.log(compChoice);
+console.log(humanChoice);
+
+
+
